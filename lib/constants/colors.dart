b6e98a899a4e4372ab1973040b4cbc583774457a
thejorabek <boyxorozovjorabek@gmail.com>
2024-09-14 23:49:52 +0500

    update

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -3,8 +3,9 @@ import 'package:flutter/material.dart';
 sealed class MyColors {
   MyColors._();
 
-  static const Color backgroundColor = Color(0xFF3A3F47);
+  static const Color backgroundColor = Color(0xFF242a33);
   static const Color activeIconColor = Color(0xFF0296E5);
   static const Color inactiveIconColor = Color(0xFF92929D);
   static const Color starColor = Color(0xFFFF8700);
+  static const Color secondaryColor = Color(0xFF3A3F47);
 }
--- lib/features/detail/detail_page.dart
+++ lib/features/detail/detail_page.dart
@@ -15,6 +15,26 @@ class DetailPage extends StatefulWidget {
   State<DetailPage> createState() => _DetailPageState();
 }
 
+final List<Map<String, String>> castMembers = [
+  {
+    'name': 'Tom Holland',
+    'imageUrl': 'https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcQxjGyv75mIabHjZS8sAsiOQn0YGeaVkIZyM1BR6l63VH9e7C_W', // Replace with actual image URL
+  },
+  {
+    'name': 'Zendaya',
+    'imageUrl': 'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcQr_o9srG0oxgqnu3ZBy9mBqmVzE9CAZycNP90EzYj09V-Qbrzv',
+  },
+  {
+    'name': 'Benedict Cumberbatch',
+    'imageUrl': 'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcTA4pJIKNOA04hZkHz2TOgIUDraO4aL8BURcO4wpNUSZNTHVymn',
+  },
+  {
+    'name': 'Brad Pitt',
+    'imageUrl': 'https://encrypted-tbn1.gstatic.com/licensed-image?q=tbn:ANd9GcTRFb9sqd8L-IvF6Uj6dxnnKOuuhfRlF2aArnDkQyudZP4ZjKBfETwCXMntRj0YOuXqQ1oB4K3971hMqAc',
+  },
+  // Add more cast members if needed
+];
+
 class _DetailPageState extends State<DetailPage> with SingleTickerProviderStateMixin {
   late TabController _tabController;
 
@@ -51,7 +71,7 @@ class _DetailPageState extends State<DetailPage> with SingleTickerProviderStateM
         body: Column(children: [
           SizedBox(
             width: width,
-            height: height * .4,
+            height: height * .39,
             child: Stack(
               children: [
                 Container(
@@ -130,18 +150,22 @@ class _DetailPageState extends State<DetailPage> with SingleTickerProviderStateM
               ],
             ),
           ),
+          SizedBox(height: height * .01),
           SizedBox(
             width: width,
-            height: height*.49,
+            height: height * .49,
             child: Column(
               children: [
                 Container(
                   child: TabBar(
+                    labelColor: Colors.white,
+                    indicatorColor: MyColors.secondaryColor,
                     controller: _tabController,
+                    dividerColor: MyColors.backgroundColor,
                     tabs: [
-                      Tab(text: 'Tab 1'),
-                      Tab(text: 'Tab 2'),
-                      Tab(text: 'Tab 3'),
+                      Tab(text: 'About Movie'),
+                      Tab(text: 'Reviews'),
+                      Tab(text: 'Cast'),
                     ],
                   ),
                 ),
@@ -149,9 +173,73 @@ class _DetailPageState extends State<DetailPage> with SingleTickerProviderStateM
                   child: TabBarView(
                     controller: _tabController,
                     children: [
-                      Center(child: Text('Content for Tab 1')),
-                      Center(child: Text('Content for Tab 2')),
-                      Center(child: Text('Content for Tab 3')),
+                      Padding(
+                        padding: const EdgeInsets.all(25),
+                        child: Text(
+                            'From DC Comics comes the Suicide Squad, an antihero team of incarcerated supervillains who act as deniable assets for the United States government, undertaking high-risk black ops missions in exchange for commuted prison sentences.',
+                            style: TextStyle(color: Colors.white)),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(25),
+                        child: Row(
+                          children: [
+                            Column(
+                              children: [CircleAvatar(), SizedBox(height: height * .015), Text('6.3', style: TextStyle(color: MyColors.activeIconColor))],
+                            ),
+                            SizedBox(width: width * .05),
+                            SizedBox(
+                              width: width * .7,
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text('Iqbal Shafiq Rozaan', style: TextStyle(color: Colors.white)),
+                                  SizedBox(height: height * .01),
+                                  const Text('From DC Comics comes the Suicide Squad, an antihero team of incarcerated supervillains who act as deniable assets for the United States government.',
+                                      style: TextStyle(color: Colors.white))
+                                ],
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(16.0),
+                        child: GridView.builder(
+                          itemCount: castMembers.length,
+                          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                            crossAxisCount: 2, // Number of items in each row
+                            mainAxisSpacing: 16.0,
+                            crossAxisSpacing: 16.0,
+                            childAspectRatio: 0.75, // Adjust the aspect ratio for the circle and text
+                          ),
+                          itemBuilder: (context, index) {
+                            return SizedBox(
+                              child: Column(
+                                mainAxisSize: MainAxisSize.min,
+                                children: [
+                                  ClipOval(
+                                    child: Image.network(
+                                      castMembers[index]['imageUrl']!,
+                                      width: 130,
+                                      height: 130,
+                                      fit: BoxFit.cover,
+                                    ),
+                                  ),
+                                  SizedBox(height: 8.0),
+                                  Text(
+                                    castMembers[index]['name']!,
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 16.0,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            );
+                          },
+                        ),
+                      ),
                     ],
                   ),
                 ),
